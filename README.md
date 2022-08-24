ea692832bb9abff5b83f8aeda4f504850b2d0190
stefangohlke <gohlke.stefan@yahoo.de>
2022-08-24 16:48:02 +0200

    Add selection page/electron forge. Update deps.

--- README.md
+++ README.md
@@ -7,30 +7,48 @@
 The strackstream Chat Viewer can help you with these tasks.
 
 ## Features
+* Enter room-id to view only chat for the room.
 * Display stackstream chat for running livestream.
 * Muted audio so it does not interfere with your own audio.
 * Window "always on top" so it stays on top of the content you are showing. 
 
 ## How to run
 
-### Preparation
+### Using Windows executable
+
+#### Preparation
+
+You can [download](https://github.com/sgohlke/stackstream-chatviewer/releases) the application as 7z archive containing the content of the application. Just extract the content to a folder of your choice.
+
+#### Display livestream chat
+1. Create and start or join a running room you want to display the chat. You need the room/case-id (the part in the url after https://stack-stream.com/case/)
+2. Run the Electron app by executing **stackstream-chatviewer.exe** file.
+3. In the input field enter the room-id and click on **Show Chat**.
+4. If asked allow the app to establish an outgoing connecection (as it tries to open the website stack-stream.com)
+5. You can move around and/or resize the window so it fits your needs
+
+### Using npm project
+
+#### Preparation
 1. Checkout this project.
 2. Run **npm install** in the root of this project (where package.json is located) to install necessary dependencies.
 3. Run the Electron app with **npm start**. You might be asked to allow cookies for stackstream, do that.
 4. You can close the app using the close-symbol on the app window.
 
-### Display livestream chat
+#### Display livestream chat
 1. Create and start or join a running room you want to display the chat. You need the room/case-id (the part in the url after https://stack-stream.com/case/)
-2. Open **main.js** and change the "roomId" constant to the room you want to display the chat.
-3. Run the Electron app with **npm start**. This will open a window showing the chat.
+2. Run the Electron app with **npm start**.
+3. In the input field enter the room-id and click on **Show Chat**.
 4. If asked allow Electon app to establish outgoing connecection (as it tries to open the website stack-stream.com)
 5. You can move around and/or resize the window so it fits your needs
 
 ## How it looks like
+
+### When you select the room
+![](roomSelection.jpg)
 
 ### What you see on the screen you are sharing
 ![](contentOnDesktop.jpg)
 
 ### What is shown in the livestream
 ![](contentInLiveStream.jpg)
-
--- main.js
+++ main.js
@@ -1,50 +1,59 @@
 // main.js
 
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const roomId = 'test-room-3'
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+let electronWindow;
+
+function createWindow() {
+  return new BrowserWindow({
     width: 400,
     height: 450,
     backgroundColor: '#FFFFFF',
     alwaysOnTop: true,
     autoHideMenuBar: true,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(`https://stack-stream.com/case/${roomId}`)
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  const contents = mainWindow.webContents
-  contents.setAudioMuted(true)
-  contents.on('did-finish-load', () => {
-      // Only show chat it div class "str-chat__container"
-    contents.insertCSS('body {visibility:hidden} .str-chat__container { visibility:visible; position:fixed; top: 0px; left: 0px; }')
-  })
-
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+function createRoomSelectionWindow() {
+  electronWindow.loadFile('index.html').then(() => {
+    electronWindow.show();
+    //electronWindow.openDevTools()
   })
-})
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+function createShowChatWindow(roomId) {
+  if (roomId) {
+    electronWindow.loadURL(`https://stack-stream.com/case/${roomId}`)
+    const contents = electronWindow.webContents
+    contents.setAudioMuted(true)
+    contents.on('did-finish-load', () => {
+      // Only show chat it div class "str-chat__container"
+      contents.insertCSS('body {visibility:hidden} .str-chat__container { visibility:visible; position:fixed; top: 0px; left: 0px; }')
+    })
+    //electronWindow.openDevTools()
+  }
+}
+
+app.on('ready', () => {
+  electronWindow = createWindow();
+  createRoomSelectionWindow();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    electronWindow = createWindow()
+  }
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+});
+
+ipcMain.on('message:showChat', (event, roomId) => {
+  createShowChatWindow(roomId);
+});
